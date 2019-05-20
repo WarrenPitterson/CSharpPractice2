2ac7aa6a56d589b25ee85993e343a927bec20ab2
warren pitterson <warrenpitterson@gmail.com>
2019-05-20 11:21:38 +0100

    initial commit

---
+++ ACM.BLTests/CustomerTest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTests
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValidity()
+        {
+            Customer customer = new Customer
+            {
+                FirstName = "Warren",
+                LastName = "Pitterson"
+            };
+
+            string expected = "Pitterson, Warren";
+
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+
+        [TestMethod]
+        public void FullNameButTheLastnameIsEmpty()
+        {
+            Customer customer = new Customer
+            {
+                LastName = "Pitterson"
+            };
+
+            string expected = "Pitterson";
+
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void FullNameButTheFirstnameIsEmpty()
+        {
+            Customer customer = new Customer
+            {
+                FirstName = "Warren"
+            };
+
+            string expected = "Warren";
+
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void StaticTest()
+        {
+            var c1 = new Customer();
+            c1.FirstName = "Warren";
+            Customer.InstanceCount += 1;
+
+            var c2 = new Customer();
+            c1.FirstName = "Liam";
+            Customer.InstanceCount += 1;
+
+            var c3 = new Customer();
+            c1.FirstName = "Anser";
+            Customer.InstanceCount += 1;
+
+            Assert.AreEqual(3, Customer.InstanceCount);
+        }
+
+        [TestMethod]
+        public void ValidateValid()
+        {
+            var customer = new Customer
+            {
+                LastName = "Pitterson",
+                EmailAddress = "warrenpittersondev@gmail.com"
+            };
+
+            var expected = true;
+
+            var actual = customer.Validate();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidateMissLastName()
+        {
+            var customer = new Customer
+            {
+                LastName = "Pitterson",
+            };
+
+            var expected = false;
+
+            var actual = customer.Validate();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+    }
+}
---
+++ ACM.BLTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BLTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM.BLTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a368b51d-b784-4113-b51b-4acfd825925b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
