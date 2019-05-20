ef271960222898ae28dffc9ea4fcf9d60b5e3004
warren pitterson <warrenpitterson@gmail.com>
2019-05-20 15:30:19 +0100

    tests added

---
+++ ACM.BLTests/CustomerRepositorytest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTests
+{
+    [TestClass]
+    public class CustomerRepositorytest
+    {
+        [TestMethod]
+        public void RetrieveValid()
+        {
+            var customerRepository = new CustomerRepository();
+            var expected = new Customer(1)
+            {
+                EmailAddress = "Warrenpitterson@gmail.com",
+                FirstName = "Warren",
+                LastName = "Pitterson"
+            };
+
+            var actual = customerRepository.Retrieve(1);
+
+            Assert.AreEqual(expected, actual);
+            }
+        
+    }
+}
